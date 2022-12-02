c37e5fabe3b8f446f136265a9eb091b6818cad82
Dominic Fellbaum <d.fellbaum@hotmail.de>
2022-12-02 11:54:58 +0100

    Day2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,12 +1,4 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01")
     val list = buildList {
---
+++ src/Day02.kt
@@ -0,0 +1,84 @@
+enum class RPC(val points: Int, val opponent: String, val self: String) {
+    ROCK(1, "A", "X"),
+    PAPER(2, "B", "Y"),
+    SCISSORS(3, "C", "Z");
+
+    companion object {
+        fun getByName(x: String): RPC {
+            for (value in values()) {
+                if (value.opponent == x || value.self == x) {
+                    return value
+                }
+            }
+            return ROCK
+        }
+    }
+}
+
+fun getPoints(pair: Pair<RPC, RPC>): Int {
+    return if (pair.first == RPC.ROCK && pair.second == RPC.PAPER
+        || pair.first == RPC.PAPER && pair.second == RPC.SCISSORS
+        || pair.first == RPC.SCISSORS && pair.second == RPC.ROCK
+    ) {
+        6
+    } else if (pair.first == pair.second) {
+        3
+    } else {
+        0
+    }
+}
+
+fun main() {
+
+    /*
+    A   ROCK
+    B   PAPER
+    C   SCISSORS
+
+    X   Rock        1
+    Y   Paper       2
+    Z   Scissors    3
+
+    loose=0
+    draw=3
+    win=6
+
+     */
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02")
+
+    val part1 = testInput
+        .map { it.split(" ") }
+        .map { Pair(RPC.getByName(it[0]), RPC.getByName(it[1])) }
+        .sumOf { getPoints(it) + it.second.points }
+
+
+    //Part 1
+    println(part1)
+
+
+    val part2 = testInput
+        .map { it.split(" ") }
+        .map {
+            val opponent = RPC.getByName(it[0])
+            if (RPC.getByName(it[1]) == RPC.ROCK) { //LOOSE
+                return@map when (opponent) {
+                    RPC.ROCK -> RPC.SCISSORS
+                    RPC.PAPER -> RPC.ROCK
+                    RPC.SCISSORS -> RPC.PAPER
+                }.points
+            } else if (RPC.getByName(it[1]) == RPC.PAPER) { //DRAW
+                return@map 3+opponent.points
+            } else { //WIN
+                return@map when (opponent) {
+                    RPC.ROCK -> RPC.PAPER
+                    RPC.PAPER -> RPC.SCISSORS
+                    RPC.SCISSORS -> RPC.ROCK
+                }.points + 6
+            }
+        }
+        .sum()
+
+    //Part 2
+    println(part2)
+}
